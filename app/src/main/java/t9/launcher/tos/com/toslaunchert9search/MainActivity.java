58327069e476ac2883a5eb5b55111d79a4dbf401
linlin <penglin.zhao@RH-F0579-SH1SW.ragent.cn>
2016-08-12 12:15:10 +0800

    add t9 code
    Signed-off-by: linlin <penglin.zhao@RH-F0579-SH1SW.ragent.cn>

---
+++ app/src/main/java/t9/launcher/tos/com/toslaunchert9search/MainActivity.java
@@ -0,0 +1,18 @@
+package t9.launcher.tos.com.toslaunchert9search;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ *t9 search 整合
+ * tos launcher
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
